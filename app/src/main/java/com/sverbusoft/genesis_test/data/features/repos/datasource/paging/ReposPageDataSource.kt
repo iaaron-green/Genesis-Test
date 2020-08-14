4794121364d3bbc6b866dbdc5228765049c8fdf7
sverbliuk <sverbliuk@ozzylogik.com>
2020-08-14 18:40:58 +0300

    Change type of datasource

--- app/src/main/java/com/sverbusoft/genesis_test/data/features/repos/datasource/paging/ReposPageDataSource.kt
+++ app/src/main/java/com/sverbusoft/genesis_test/data/features/repos/datasource/paging/ReposPageDataSource.kt
@@ -1,6 +1,6 @@
 package com.sverbusoft.genesis_test.data.features.repos.datasource.paging
 
-import androidx.paging.PositionalDataSource
+import androidx.paging.PageKeyedDataSource
 import com.sverbusoft.genesis_test.data.features.repos.datasource.remote.ReposRemoteDataSource
 import com.sverbusoft.genesis_test.data.features.repos.model.ReposResponseItem
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -8,30 +8,35 @@ import io.reactivex.schedulers.Schedulers
 
 class ReposPageDataSource(
     private val dataSource: ReposRemoteDataSource
-) : PositionalDataSource<ReposResponseItem>() {
-    var id: Int = 0;
-
+) : PageKeyedDataSource<Int, ReposResponseItem>() {
     override fun loadInitial(
-        params: LoadInitialParams,
-        callback: LoadInitialCallback<ReposResponseItem>
+        params: LoadInitialParams<Int>,
+        callback: LoadInitialCallback<Int, ReposResponseItem>
     ) {
-        fetchData(id) {
-            id = it[it.lastIndex].id
-            callback.onResult(it, 0)
+        fetchData(0, params.requestedLoadSize) {
+            callback.onResult(it, null, 1)
         }
     }
 
-    override fun loadRange(
-        params: LoadRangeParams,
-        callback: LoadRangeCallback<ReposResponseItem>
+    override fun loadAfter(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, ReposResponseItem>
     ) {
-        fetchData(id) {
-            id = it[it.lastIndex].id
-            callback.onResult(it)
+        fetchData(params.key, params.requestedLoadSize) {
+            callback.onResult(it, params.key + 1)
         }
     }
 
-    private fun fetchData(pageSize: Int, callback: (List<ReposResponseItem>) -> Unit) {
+    override fun loadBefore(
+        params: LoadParams<Int>,
+        callback: LoadCallback<Int, ReposResponseItem>
+    ) {
+        fetchData(params.key, params.requestedLoadSize) {
+            callback.onResult(it, params.key - 1)
+        }
+    }
+
+    private fun fetchData(page: Int, pageSize: Int, callback: (List<ReposResponseItem>) -> Unit) {
         var disposable = dataSource.getUserRepos("qwe")
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
